6b8ac95aedb0b63bfe0fafc32f0eb242a358f690
gokul087 <gokulpalanivel06@gmail.com>
2025-01-06 13:06:43 +0530

    server index update

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json({extended: true}))
 // Url builtin middlewares it is used to handle the form methods:
 app.use(express.urlencoded({extended: true}))
 // Cross origin resource sharing(cors) Third party middlewares:
-app.use(cors({credentials: true, origin: process.env.CLIENT_URL}))
+app.use(cors({credentials: true, origin: "http://localhost:3000"}))
 
 // Upload routes and middleware:
 app.use(upload())
